e49c2301bece2da6237f420714b2e4b2cec73fe0
Fra619-ghost <fxaaguilera@uamv.edu.ni>
2024-04-03 16:52:28 -0600

    Se agrega el coso este

---
+++ otrapractica.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+
+int main(int argc, char const *argv[])
+{
+    printf("\nBueno esta es simplemente una segunda practica");
+    printf("\nBienvenido al centro de usuario de la UAM");
+    printf("\nIngresa sus datos personales");
+    printf("\nPor favor sea amable pero bien amableloso de ingresar los digitos que desee");
+    printf("\nPor favor vuelva a ingresar los mismos digitos para ingresar");
+
+    
+}
